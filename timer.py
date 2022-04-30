a5e2ddb2a5f95d0b598673b96f613011a3855cf6
Whiffles <68132950+whifflesdev@users.noreply.github.com>
2022-04-30 15:19:33 -0700

    Update timer.py
    finished timer

--- timer.py
+++ timer.py
@@ -1,3 +1,4 @@
+
 import time
 from tkinter import *
 from tkinter import messagebox
@@ -35,5 +36,41 @@ secondTextbox.place(x=270, y=180)
 
 def runTimer():
     try:
-        clockTime = int(hourString.get()*3600) + int(minuteString.get()*60) + int(secondString.get())
-        
+        clockTime = int(hourString.get())*3600 + int(minuteString.get())*60 + int(secondString.get())
+    except:
+        print("Can't do that")
+
+    while(clockTime > -1):
+
+        totalMinutes, totalSeconds = divmod(clockTime, 60)
+
+        totalHours = 0
+        if(totalMinutes > 60):
+            totalHours, totalMinutes = divmod(totalMinutes, 60)
+
+
+        hourString.set("{0:2d}".format(totalHours))
+        minuteString.set("{0:2d}".format(totalMinutes))
+        secondString.set("{0:2d}".format(totalSeconds))
+
+
+
+        ### Update the interface
+        clockWindow.update()
+        time.sleep(1)
+
+        ### Let user know if the timer has expired
+        if(clockTime == 0):
+            messagebox.showinfo("HEY!", "Your time has run out!")
+
+
+        clockTime -= 1
+
+
+### adding button
+setTimeButton = Button(clockWindow, text='Set Time', bd='5', command=runTimer)
+setTimeButton.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+
+### Keep looping
+clockWindow.mainloop()
